9a5db9219bd10fc31c93100e7e6f0e2ae8d962f2
abo1007 <1003208172@qq.com>
2020-04-28 12:57:29 +0800

    update

--- src/photo.js
+++ src/photo.js
@@ -5,45 +5,40 @@ const Pclass = [
     { title: "前端后端" , id:4 },
     { title: "数据分析" , id:5 }
 ];
-const photoInfo = [
-    [
-        {id:1, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/1.jpg'},
-        {id:2, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/2.jpg'},
-        {id:3, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/3.jpg'},
-        {id:4, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/4.jpg'},
-        {id:5, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/5.jpg'},
-        {id:6, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/6.jpg'},
-        {id:7, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/7.jpg'},
-        {id:8, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/8.jpg'},
-        {id:9, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/9.jpg'},
-        {id:10, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/10.jpg'}
-    ],
-    [
-        {id:1, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/1.jpg'},
-        {id:2, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/2.jpg'},
-        {id:3, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/3.jpg'}
-    ],
-    [
-        {id:2, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/2.jpg'},
-        {id:3, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/3.jpg'},
-        {id:4, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/4.jpg'}
-    ],
-    [
-        {id:4, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/4.jpg'},
-        {id:5, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/5.jpg'},
-        {id:6, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/6.jpg'}
-    ],
-    [
-        {id:6, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/6.jpg'},
-        {id:7, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/7.jpg'},
-        {id:8, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/8.jpg'}
-    ],
-    [
-        {id:8, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/8.jpg'},
-        {id:9, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/9.jpg'},
-        {id:10, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/10.jpg'}
-    ]
-]
+
+const mysql = require('mysql');
+var dbconfig = require('./database.config');
+
+const db = mysql.createConnection(dbconfig);
+db.connect((err) => {
+    if(err) {
+        console.log("失败");
+        throw err;
+    }
+    console.log('连接成功');
+})
+
+var photoInfo = [];
+
+var sqlsay = "SELECT * FROM `photoinfo`";
+
+for(let i = 0; i <= 5 ; i++) {
+    if(i == 0){
+        db.query(sqlsay, (err, result) => {
+            if(err) throw err;
+            photoInfo[i] = result;
+        })
+        continue;
+    }
+    let sqlsay1 = "SELECT * FROM `photoinfo` WHERE `class` = " + i;
+    
+    db.query(sqlsay1, (err, result) => {
+        if(err) throw err;
+        photoInfo[i] = result;
+        
+    })
+}
+db.end();
 
 module.exports = {
     Pclass,
---
+++ src/废弃的photo.js
@@ -0,0 +1,39 @@
+// const photoInfo = [
+//     [
+//         {id:1, title:'标题1', content:'内容1', url: 'http://127.0.0.1:3008/api/public/images/photo/1.jpg'},
+//         {id:2, title:'标题2', content:'内容2', url: 'http://127.0.0.1:3008/api/public/images/photo/2.jpg'},
+//         {id:3, title:'标题3', content:'内容3', url: 'http://127.0.0.1:3008/api/public/images/photo/3.jpg'},
+//         {id:4, title:'标题4', content:'内容4', url: 'http://127.0.0.1:3008/api/public/images/photo/4.jpg'},
+//         {id:5, title:'标题5', content:'内容5', url: 'http://127.0.0.1:3008/api/public/images/photo/5.jpg'},
+//         {id:6, title:'标题6', content:'内容6', url: 'http://127.0.0.1:3008/api/public/images/photo/6.jpg'},
+//         {id:7, title:'标题7', content:'内容7', url: 'http://127.0.0.1:3008/api/public/images/photo/7.jpg'},
+//         {id:8, title:'标题8', content:'内容8', url: 'http://127.0.0.1:3008/api/public/images/photo/8.jpg'},
+//         {id:9, title:'标题9', content:'内容9', url: 'http://127.0.0.1:3008/api/public/images/photo/9.jpg'},
+//         {id:10, title:'标题10', content:'内容10', url: 'http://127.0.0.1:3008/api/public/images/photo/10.jpg'}
+//     ],
+//     [
+//         {id:1, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/1.jpg'},
+//         {id:2, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/2.jpg'},
+//         {id:3, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/3.jpg'}
+//     ],
+//     [
+//         {id:2, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/2.jpg'},
+//         {id:3, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/3.jpg'},
+//         {id:4, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/4.jpg'}
+//     ],
+//     [
+//         {id:4, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/4.jpg'},
+//         {id:5, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/5.jpg'},
+//         {id:6, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/6.jpg'}
+//     ],
+//     [
+//         {id:6, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/6.jpg'},
+//         {id:7, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/7.jpg'},
+//         {id:8, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/8.jpg'}
+//     ],
+//     [
+//         {id:8, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/8.jpg'},
+//         {id:9, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/9.jpg'},
+//         {id:10, title:'', content:'', url: 'http://127.0.0.1:3008/api/public/images/photo/10.jpg'}
+//     ]
+// ]
